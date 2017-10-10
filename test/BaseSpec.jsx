61041b48ebb01930ad188005e3b3e26fdcd1f698
3axap4eHko <3axap4eHko@gmail.com>
2017-10-10 12:47:32 -0400

    removed legacy tests

--- test/BaseSpec.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { renderIntoDocument } from 'react-dom/test-utils';
-
-describe('Base test suite', () => {
-  it('Render test', () => {
-    renderIntoDocument(<div>test</div>);
-  });
-});
