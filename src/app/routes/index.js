a742c45dc97ea9062faf27770fe45c4d8b423ce8
3axap4eHko <3axap4eHko@gmail.com>
2017-11-01 14:57:19 -0400

    simplify routes

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -3,12 +3,6 @@ import render from '../utils/render';
 
 const router = Express.Router();
 
-router.get('/', (req, res) => {
-  render(req, res, {});
-});
-
-router.get('/:slug', (req, res) => {
-  render(req, res, {});
-});
+router.get(['/', '/:slug'], (req, res) => render(req, res, {}));
 
 export default router;
